0888736f9fb9a6a5a81e92cf050115d6e3ebd078
Lucrecia Cáceres Ruiz <108938705+Lucre-Caceres@users.noreply.github.com>
2023-06-21 10:38:17 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from "./components/Navbar"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1 greeting="hola" />
+    </div>
+  );
+};
+
+export default App
